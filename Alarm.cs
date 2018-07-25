e260d9369d60e6c861f19bee2bce6ebe48dfe5e9
Rajko Vrga <rajkovrga@yahoo.com>
2018-07-25 12:37:05 +0200

    My first C# project

---
+++ Alarm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MyAlarm
+{
+  
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alarm Time");
+            string time = Console.ReadLine();
+            Console.WriteLine("youtube link(alarm sound)b");
+            string link = Console.ReadLine();
+            int nowMinute = DateTime.Now.Minute;
+            int nowHours = DateTime.Now.Hour;
+            DateTime wanted = DateTime.Parse(time);
+
+            while (nowMinute != wanted.Minute || nowHours != wanted.Hour)
+            {
+                nowMinute = DateTime.Now.Minute;
+                nowHours = DateTime.Now.Hour;
+                Thread.Sleep(1000);
+            }
+
+            ProcessStartInfo chrome = new ProcessStartInfo("chrome.exe")
+            {
+               Arguments = link
+            };
+           Process.Start(chrome);
+        }
+
+       
+    }
+}
